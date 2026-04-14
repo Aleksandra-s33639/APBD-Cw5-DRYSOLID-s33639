8c22f1fe0e4cd28a07f9c36305db32feb4af275e
Aleksandra Jurszo <s33639@pjwstk.edu.pl>
2026-04-14 21:52:49 +0200

    Added SupportFeeCalculator class

---
+++ APBD-Cw5-DRYSOLID-s33639/LegacyRenewalApp/Pricing/SupportFeeCalculator.cs
@@ -0,0 +1,24 @@
+﻿namespace LegacyRenewalApp.Pricing
+{
+    public class SupportFeeCalculator
+    {
+        public decimal Calculate(string planCode, bool includePremiumSupport, ref string notes)
+        {
+            if (!includePremiumSupport)
+                return 0;
+
+            decimal supportFee = 0;
+
+            if (planCode == "START")
+                supportFee = 250m;
+            else if (planCode == "PRO")
+                supportFee = 400m;
+            else if (planCode == "ENTERPRISE")
+                supportFee = 700m;
+
+            notes += "premium support included; ";
+
+            return supportFee;
+        }
+    }
+}
